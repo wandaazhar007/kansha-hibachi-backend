878257100be40d4c4f3fa78bbc828a26a9e53fd0
wanda azhar <wandaazhar@gmail.com>
2023-06-20 13:44:51 -0400

    module products

--- controllers/productController.js
+++ controllers/productController.js
@@ -62,3 +62,39 @@ export const getProducts = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 }
+
+export const createProduct = async (req, res) => {
+  if (req.files === null) return res.status(400).json({ msg: "No Image Uploaded" });
+  const name = req.body.name;
+  const slug = req.body.slug;
+  const price = req.body.price;
+  const image = req.files.image;
+  const desc = req.body.desc;
+  const categoryId = req.body.categoryId;
+  const fileSize = image.data.length;
+  const ext = path.extname(image.name);
+  const fileName = image.md5 + ext;
+  const url = `${req.protocol}://${req.get("host")}/images/products/${fileName}`;
+  const allowedType = ['.png', '.jpg', '.jpeg'];
+
+  if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: "Invalid Images" });
+  if (fileSize > 5000000) return res.status(422).json({ msg: "Image must be less than 5 MB" });
+
+  image.mv(`./public/images/products/${fileName}`, async (err) => {
+    if (err) return res.status(500).json({ msg: err.message });
+    try {
+      await Products.create({
+        name: name,
+        slug: slug,
+        price: price,
+        image: fileName,
+        urlImage: url,
+        desc: desc,
+        categoryId: categoryId
+      });
+      res.status(200).json({ msg: "Products has been saved" });
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getProducts } from '../controllers/productController.js';
+import { createProduct, getProducts } from '../controllers/productController.js';
 // import { verifyUser } from '../middleware/AuthUser.js';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
+router.post('/products', createProduct);
 
 export default router;
