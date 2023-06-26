b62e60794d70ce7b002cc2425dd6aa0d4b053503
wanda azhar <wandaazhar@gmail.com>
2023-06-26 02:10:14 -0400

    search products

--- config/db_config.js
+++ config/db_config.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
   port: process.env.DB_PORT
 });
 
