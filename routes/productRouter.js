c57f8d0791c4607fc1f48317c6e93e0f08e95a63
wanda azhar <wandaazhar@gmail.com>
2023-08-02 02:58:00 -0400

    update products with images

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import { createProduct, deleteProducts, getProductById, getProducts, seacrhProducts, updateProduct, getProductCart, getAllProducts } from '../controllers/productController.js';
+import { createProduct, deleteProducts, getProductById, getProducts, updateProduct, getProductCart, getAllProducts, getProductsPerCategory, searchProducts } from '../controllers/productController.js';
 // import { verifyUser } from '../middleware/AuthUser.js';
 
 const router = express.Router();
 
 router.get('/products', getProducts);
 router.get('/all-products', getAllProducts);
+router.get('/products-per-category', getProductsPerCategory);
 router.get('/products/:id', getProductById);
 router.get('/products-cart/:id', getProductCart);
-router.get('/search-products', seacrhProducts);
+router.get('/search-products', searchProducts);
 router.post('/products', createProduct);
-router.patch('/products/:slug', updateProduct);
-router.delete('/products/:slug', deleteProducts);
+router.patch('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProducts);
 
 export default router;
