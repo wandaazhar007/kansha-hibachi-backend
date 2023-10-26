8af3d59b368ab692a6cd4e6bd90419d4d8434a4f
wanda azhar <wandaazhar@gmail.com>
2023-10-26 01:50:12 -0400

    refactoring code

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.use(session({
 //   credentials: true,
 //   origin: '*'
 // }));
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: ['http://localhost:5173', 'http://localhost:4173', 'http://localhost:3000', 'http://localhost:3003', 'http://kanshamissouri.com', 'http://admin.kanshamissouri.com'] }));
+// app.use(cors({ credentials: true, origin: '*' }));
 
 app.use(cookieParser());
 app.use(express.json());
